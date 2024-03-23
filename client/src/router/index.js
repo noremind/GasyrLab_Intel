2477476f52acf942d6dcd3c768e22fa28dcd1ffa
norem <mara19851234@gmail.com>
2024-03-23 20:28:06 +0500

    Update for github

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -11,10 +11,12 @@ const router = createRouter({
 		{
 			path: '/detail-page',
 			name: 'details',
-			// route level code-splitting
-			// this generates a separate chunk (About.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
 			component: () => import('@/views/DetailsPage.vue')
+		},
+		{
+			path: '/about',
+			name: 'about',
+			component: () => import('@/views/AboutPage.vue')
 		}
 	]
 })
