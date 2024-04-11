bf9acf6becec9b3601c368324168a71b2aca8e9b
IssaNartay <isanartayuly@gmail.com>
2024-04-11 13:58:21 +0600

    updHash

--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,22 +1,22 @@
 <template>
-  <!-- <Intro></Intro> -->
+  <Intro></Intro>
   <Header></Header>
-  <!-- <aboutCEO></aboutCEO> -->
-  <!-- <Greeting></Greeting> -->
+  <Greeting></Greeting>
   <Courses></Courses>
+  <Sponsors></Sponsors>
+  <aboutCEO></aboutCEO>
   <OurTeam></OurTeam>
-  <!-- <Sponsors></Sponsors> -->
-  <!-- <Subscription></Subscription> -->
-  <!-- <Waves></Waves> -->
-  <!-- <Footer></Footer> -->
+  <Subscription></Subscription>
+  <Waves></Waves>
+  <Footer></Footer>
 </template>
 
 <script setup>
 import Intro from '@/components/ScreenSaver.vue'
 import Header from '@/components/HeaderMain.vue'
 import Greeting from '@/components/GreetingMain.vue'
-import Sponsors from '@/components/HorizontalScrollGsap.vue'
 import Courses from '@/components/OurCourses.vue'
+import Sponsors from '@/components/HorizontalScrollGsap.vue'
 import aboutCEO from '@/components/AboutCEO.vue'
 import OurTeam from '@/components/OurTeam.vue'
 import Subscription from '@/components/SubscriptionForm.vue'
